b03e89e0e480f08af85b1dea086cced816101f78
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-07-01 15:46:50 +0200

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="container-fluid">
+    <div class="row">
+        <p class="lead">Awodwa Portfolio &copy; <span class="shadow" > {{ currentYear }} </span></p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "FooterComp",
+    data(){
+        return {
+            currentYear: new Date().getUTCFullYear()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
